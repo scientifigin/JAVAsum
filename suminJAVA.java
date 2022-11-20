9f14d865424c603943c71991cb5b43f3fbe46262
GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>
2022-11-20 18:08:43 +0530

    Create suminJAVA.java
    Signed-off-by: GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>

---
+++ suminJAVA.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The sum of three numbers is =");
+        int num1 = 54646246;
+        int num2 = 139579349;
+        int num3 = 23283462;
+        int sum = num1 - num2 - num3;
+        System.out.println(sum);
+    }
+}
+
