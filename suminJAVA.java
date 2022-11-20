154d0be38e99bd65ac1da560cf84f475be7fdb4f
GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>
2022-11-20 18:13:43 +0530

    Update suminJAVA.java
    Signed-off-by: GOPAL SAHA <112541952+gopalsahagsdev@users.noreply.github.com>

--- suminJAVA.java
+++ suminJAVA.java
@@ -1,9 +1,24 @@
-package com.company;
+package scientifigin;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("The sum of three numbers is =");
+        int num1 = 54646246;
+        int num2 = 139579349;
+        int num3 = 23283462;
+        int sum = num1 - num2 - num3;
+        System.out.println(sum);
+    }
+}
+
+package scientifigin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.print("The sum of three numbers is = ");
         int num1 = 54646246;
         int num2 = 139579349;
         int num3 = 23283462;
